99528f4f4087ef7dde7b16f7ecaec99ec55654e7
LebniZaabdi <54181939+LebniZaabdi@users.noreply.github.com>
2022-06-13 08:46:33 -0500

    Add files via upload
    Renderizar en Unity las imágenes de una cámara de videovigilancia en "Tiempo Real"

---
+++ Imagen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+
+public class Imagen : MonoBehaviour
+{
+    public InputField IPs;
+    public string geturl;
+    public Renderer rend;
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        StartCoroutine(GetTexture());
+        var t = Task.Run(async delegate
+        {
+            await Task.Delay(90);
+            return 42;
+        });
+        t.Wait();
+    }
+    IEnumerator GetTexture()
+    {
+
+        while (true)
+        {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(geturl);
+            yield return www.SendWebRequest();
+            if (www.isDone == true)
+            {
+                rend = GetComponent<Renderer>();
+                rend.material.mainTexture = DownloadHandlerTexture.GetContent(www);
+                UnityWebRequest.ClearCookieCache();
+            }
+            StopAllCoroutines();
+        }
+    }
+}
